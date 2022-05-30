6071c08fbbc5704e47d7009ba5d8fbb439f08fa0
syahirah <91903547+haysira@users.noreply.github.com>
2022-05-31 00:08:56 +0800

    Add files via upload

---
+++ LabAssignment7/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>Noor Syahirah 207542 Lab7</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1>Welcome to Autos Database</h1>
+  <a href="login.php">Please Log in</a>
+  <p>Attempt to go to <a href="autos.php">game.php</a> without logging in -
+    it should fail with an error message</p>
+    <a href="#">Specification for this Application</a>
+  </body>
+  </html>
